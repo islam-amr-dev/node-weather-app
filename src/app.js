8134707bc2d2dee87da20b1dc1cac872e086fd52
islam-amr-dev <islamtaeb@gmail.com>
2020-03-16 11:09:24 -0700

    Init commit

---
+++ src/app.js
@@ -0,0 +1,102 @@
+const express = require("express");
+const path = require("path");
+const hbs = require("hbs");
+const geocode= require('./utils/geocode')
+const forecast= require('./utils/forecast')
+
+const viewPath = path.join(__dirname,'../templates/views');
+const partialsPath  = path.join(__dirname,'../templates/partials')
+const publicDirectoryPath =path.join(__dirname,'../public')
+
+const app =express();
+
+app.set('view engine','hbs');
+app.set('views',viewPath);
+hbs.registerPartials(partialsPath);
+
+app.use(express.static(publicDirectoryPath));
+
+app.get('',(req,res)=>{
+    res.render('index',{
+        title:'weather',
+        name:'islam amr'
+    })
+})
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title:'about',
+        name:'islam amr'
+    })
+})
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        title:'help',
+        name:'islam amr'
+    })
+})
+app.get('/weather',(req,res)=>{
+    if(!req.query.address){
+        
+        return res.send({
+            error:"no address is porvided"
+        })
+    }
+    geocode(req.query.address,(error,{longitude,latitude,location}={})=>{
+        if(error){
+            return res.send({error})
+        }
+       forecast(longitude,latitude,(error,forecastData)=>{
+           if(error){
+               return res.send({error})
+           }
+           res.send({
+            forecast:  forecastData,
+            location,
+            address:req.query.address
+        })
+       })
+    })
+    
+})
+app.get('/*',(req,res)=>{
+    res.render('404',{
+        title:"404",
+        name:"islam amr",
+        errormessage:"page not found"
+    })
+})
+app.get('/help/*',(req,res)=>{
+    res.render('404',{
+        title:"404",
+        helpText:"some help data",
+        name:"help article not found"
+    })
+})
+app.listen(3000)
+
+
+
+/*const address= process.argv[2] 
+const geocode = require("./utils/geocode");
+const forecast= require("./utils/forecast");
+
+
+if(!address){
+    return console.log("enter a place to get information ya haywan");
+}else{
+    geocode(address,(err,{longitude,latitude,location})=>{
+        if(err){
+            return console.log(err);
+        }
+            forecast(longitude,latitude,(err,forcastdata)=>{
+                if(err){
+                    return console.log(err);
+                }
+                console.log(location);
+                console.log(forcastdata);
+                
+            })
+        
+    })
+    
+}*/
---
+++ src/utils/geocode.js
@@ -0,0 +1,19 @@
+const request = require ("request");
+const geocode = (address,callback)=>{
+    const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + encodeURIComponent(address) + ".json?access_token=pk.eyJ1IjoiaXNsYW10YWViIiwiYSI6ImNrNndpYmV1MDBieWwzZXF2OGh1NXZkamoifQ.NsPfJokU6qSJBgllWoDMZA";
+    request({url,json:true},(error,{body})=>{
+        if(error){
+            callback("unable to connect to  location services",undefined);
+        }else if(body.features.length === 0){
+            callback("un able to find location .Try another one .",undefined);
+        }else{
+            callback(undefined,{
+                longitude: body.features[0].center[0],
+                latitude : body.features[0].center[1],
+                location : body.features[0].place_name
+            })
+        }
+    })
+} 
+
+module.exports=geocode;
